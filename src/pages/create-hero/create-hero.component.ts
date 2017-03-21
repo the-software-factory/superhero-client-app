261ef8f39f1119d18daf656adb58f49bea0ea746
Aleksey Alekseevich Lyulin <codwizard@gmail.com>
2017-03-21 12:39:53 +0100

    Template.driven form

--- src/pages/create-hero/create-hero.component.ts
+++ src/pages/create-hero/create-hero.component.ts
@@ -1,7 +1,26 @@
 
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Superhero } from '../../models/superhero';
 
 @Component({
   templateUrl: 'create-hero.template.html'
 })
-export class CreateHeroPage {}
+export class CreateHeroPage {
+
+  superHero: Superhero = new Superhero();
+
+  onSubmit(form: NgForm) {
+    console.log(`The form is ${form.valid ? 'VALID' : 'INVALID'}`);
+
+    if (form.invalid) {
+      return;
+    }
+
+    this.superHero.hasCloak = !!this.superHero.hasCloak;
+
+    console.log(this.superHero);
+  }
+
+}
